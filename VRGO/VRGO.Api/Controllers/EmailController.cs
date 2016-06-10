9c9f2251a431348675612ab5963f91fb925e3ad4
Alexander Mishulkov <alexander.mishulkov@notime.eu>
2016-06-10 17:01:50 +0200

    email sending

---
+++ VRGO/VRGO.Api/Controllers/EmailController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Http;
+using VRGO.Api.Models;
+using VRGO.Api.Properties;
+
+namespace VRGO.Api.Controllers
+{
+    public class EmailController : ApiController
+    {
+	    private const string SmtpServer = "smtp.gmail.com";
+	    private const int SmtpPort = 587;
+	    private const string SmtpLogin = "vrgoby@gmail.com";
+	    private const string SmtpPassword = "qwasZX12";
+
+        public IHttpActionResult Post(EmailViewModel viewModel)
+        {
+			if (!ModelState.IsValid)
+			{
+				return BadRequest(ModelState);
+			}
+
+			try
+	        {
+				SendEmail(viewModel);
+		        return Ok();
+	        }
+	        catch (Exception e)
+	        {
+		        return InternalServerError();
+	        }
+        }
+
+	    private void SendEmail(EmailViewModel viewModel)
+	    {
+		    var client = new SmtpClient
+		    {
+			    Host = SmtpServer,
+			    EnableSsl = true,
+			    Port = SmtpPort,
+			    DeliveryMethod = SmtpDeliveryMethod.Network,
+			    UseDefaultCredentials = false
+		    };
+		    client.Credentials = new NetworkCredential(SmtpLogin, SmtpPassword);
+
+			var mailMessage = new MailMessage(new MailAddress(viewModel.EmailAddress, viewModel.Name), new MailAddress(SmtpLogin, "Info"))
+		    {
+			    Subject = Resources.EmailSubject,
+			    Body = $"{viewModel.Message}{Environment.NewLine}Номер телефона: {viewModel.PhoneNumber}{Environment.NewLine}Email адрес: {viewModel.EmailAddress}"
+		    };
+
+		    client.Send(mailMessage);
+		}
+    }
+}
---
+++ VRGO/VRGO.Api/Models/EmailViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VRGO.Api.Models
+{
+	public class EmailViewModel
+	{
+		[Required]
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		public string PhoneNumber
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		public string EmailAddress
+		{
+			get;
+			set;
+		}
+
+		[Required]
+		public string Message
+		{
+			get;
+			set;
+		}
+	}
+}
