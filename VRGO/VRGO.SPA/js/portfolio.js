1f04b6bd4e4a0d6ddfbf22f65d53327c6660e646
Alexander <Mishulkov>
2016-09-19 23:32:31 +0300

    added new portfolio and customers

--- VRGO/VRGO.SPA/js/portfolio.js
+++ VRGO/VRGO.SPA/js/portfolio.js
@@ -8,4 +8,9 @@ $(function(){
 		event.preventDefault()
 		blueimp.Gallery($('#a1qa-camping-portfolio-links a'), $('#blueimp-gallery').data())
 	})
+
+	$('#grand-fiesta-company-portfolio').on('click', function (event) {
+		event.preventDefault()
+		blueimp.Gallery($('#grand-fiesta-company-portfolio-links a'), $('#blueimp-gallery').data())
+	})
 })
